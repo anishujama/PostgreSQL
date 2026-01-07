9525a4620b2c2730a37db7be87d280e0b0e9e260
Anishujama khan <anishkh9374@gmail.com>
2026-01-07 20:43:14 +0530

    Add various SQL join examples for customer and payment

--- SQL/Join.sql
+++ SQL/Join.sql
@@ -2,3 +2,18 @@ select c.first_name, p.amount,p.mode
 from customer as c
 full outer join payment as p
 on c.customer_id = p.customer_id
+
+select c.first_name, p.amount,p.mode
+from customer as c
+right join payment as p
+on c.customer_id = p.customer_id
+
+select c.first_name, p.amount,p.mode
+from customer as c
+left join payment as p
+on c.customer_id = p.customer_id
+
+select c.first_name, p.amount,p.mode
+from customer as c
+inner join payment as p
+on c.customer_id = p.customer_id
