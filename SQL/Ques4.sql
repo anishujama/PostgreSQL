f7c77975e2ede32b202b50a111a465f2818a3936
Anishujama khan <anishkh9374@gmail.com>
2026-01-10 21:54:31 +0530

    Add Applications table and sample data for candidates
    Created Applications table and inserted sample data for candidates with required skills.

--- SQL/Ques4.sql
+++ SQL/Ques4.sql
@@ -1,5 +1,22 @@
+CREATE TABLE Applications (
+candidate_id int,
+skills varchar);
+
+INSERT INTO Applications(candidate_id,skills)
+VALUES
+(101, 'Power BI'), (101, 'Python'), (101, 'SQL'), (102, 'Tableau'), (102, 'SQL'),
+(108, 'Python'), (108, 'SQL'), (108, 'Power BI'), (104, 'Python'), (104, 'Excel')
+
+
+Find the candidates best suited for an open Data Science job. Find candidates who are proficient
+in Python, SQL, and Power BI.
+Write a query to list the candidates who possess all of the required skills for the job. Sort the output
+by candidate ID in ascending order. 
+
+  
 select candidate_id, count(skills) as skillcount from Applications
 where skills in ('Python','SQL','Power BI')
 group by candidate_id
 having count (skills) = 3
 order by candidate_id asc
+
