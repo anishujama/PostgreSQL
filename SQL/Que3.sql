a511bdcf71c219fb96245995f2f3c232b03f8868
Anishujama khan <anishkh9374@gmail.com>
2026-01-10 21:27:37 +0530

    Add files via upload

---
+++ SQL/Que3.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Products (
+Order_date date,
+Sales int );
+
+INSERT INTO Products(Order_date,Sales)
+VALUES
+('2021-01-01',20), ('2021-01-02',32), ('2021-02-08',45), ('2021-02-04',31),
+('2021-03-21',33), ('2021-03-06',19), ('2021-04-07',21), ('2021-04-22',10)
