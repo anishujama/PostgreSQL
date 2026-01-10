6c125d33d41926098f2ee366329324ad3782168e
Anishujama khan <anishkh9374@gmail.com>
2026-01-10 21:32:10 +0530

    Create Products table and add sales query
    Added SQL query to find monthly sales and sort by descending order.

--- SQL/Que3.sql
+++ SQL/Que3.sql
@@ -5,4 +5,13 @@ Sales int );
 INSERT INTO Products(Order_date,Sales)
 VALUES
 ('2021-01-01',20), ('2021-01-02',32), ('2021-02-08',45), ('2021-02-04',31),
-('2021-03-21',33), ('2021-03-06',19), ('2021-04-07',21), ('2021-04-22',10)
+
+('2021-03-21',33), ('2021-03-06',19), ('2021-04-07',21), ('2021-04-22',10) 
+
+find mothly sales and sort it by desc order
+  
+select extract(year from Order_date) as Years , to_char(order_date,'Mon') as months,
+sum(sales) as totalsales
+from Products
+group by 1,2
+order by totalsales desc
